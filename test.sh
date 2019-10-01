ee00dd13acaf6254f1225f3b75e4ef467eb529b0
Vincent Perez <perezv815@gmail.com>
2019-10-01 18:28:21 -0400

    Add minimalist test script

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cc -std=c99 -Wall -g parsing.c mpc.c -ledit -lm -o parsing && ./parsing < test_input.txt > tmp.txt
+
+# TODO: try to remove the first 3 lines of the comparison, with `tail -n +4 <file>
+diff tmp.txt test_expected.txt
+if [ $? -ne 0 ]
+then
+  echo "test failed"
+  rm tmp.txt
+  exit 1
+else
+  echo "test successful"
+  rm tmp.txt
+  exit 0
+fi
